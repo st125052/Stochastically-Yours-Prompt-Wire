0b38506344bcb20ab5e7c2e648b3c5ade69c5c89
Swaraj Bhanja <st125052@ait.ac.th>
2025-05-10 18:27:19 +0700

    Expose Cloud Watch To Home Routes

--- app/weaviate/routes/home.py
+++ app/weaviate/routes/home.py
@@ -1,8 +1,12 @@
 from flask import Blueprint, jsonify
 from weaviate_client.client import client
+from utils.cloudwatch_utils import get_logger, publish_metric
 
 home_bp = Blueprint("home", __name__)
+logger = get_logger()
 
 @home_bp.route("/", methods=["GET"])
 def home():
+    logger.info("Weaviate home endpoint accessed")
+    publish_metric("WeaviateHome", 1)
     return jsonify({"message": "Welcome to the Weaviate API"}), 200
